30420fed39b7beb5c8548abf4e89557e7044aaa6
Luis Camacho <camacho.le@gmail>
2019-07-25 10:28:02 -0600

    Form navigation was updated

--- src/Controller/ProjectRequestController.php
+++ src/Controller/ProjectRequestController.php
@@ -11,133 +11,151 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Services\Utils\FileManager;
 
-
 /**
  * @Route("/project/request")
  */
-class ProjectRequestController extends AbstractController
-{
-    /**
-     * @Route("es/", name="project_request_index", methods={"GET"})
-     */
-    public function index(): Response
-    {
-        $projectRequests = $this->getDoctrine()
+class ProjectRequestController extends AbstractController {
+
+  /**
+   * @Route("es/", name="project_request_index", methods={"GET"})
+   */
+  public function index(): Response {
+    $projectRequests = $this->getDoctrine()
             ->getRepository(ProjectRequest::class)
             ->findAll();
 
-        return $this->render('project_request/index.html.twig', [
-            'project_requests' => $projectRequests,
-        ]);
-    }
-    
-    /**
-     * @Route("/admin", name="project_request_index_admin", methods={"GET"})
-     */
-    public function indexAdmin(): Response
-    {
-        $projectRequests = $this->getDoctrine()
+    return $this->render('project_request/index.html.twig', [
+                'project_requests' => $projectRequests,
+    ]);
+  }
+
+  /**
+   * @Route("/admin", name="project_request_index_admin", methods={"GET"})
+   */
+  public function indexAdmin(): Response {
+    $projectRequests = $this->getDoctrine()
             ->getRepository(ProjectRequest::class)
             ->findAll();
 
-        return $this->render('project_request/index.html.twig', [
-            'project_requests' => $projectRequests,
-        ]);
+    return $this->render('project_request/index.html.twig', [
+                'project_requests' => $projectRequests,
+    ]);
+  }
+
+  /**
+   * @Route("/new", name="project_request_new", methods={"GET","POST"})
+   */
+  public function new(Request $request, FileManager $fileManager): Response {
+    $projectRequest = new ProjectRequest();
+    $form = $this->createForm(ProjectRequestType::class, $projectRequest);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+
+      $extInstitutionsAuthorizationUploadedFiles = $form->get("extInstitutionsAuthorizationFiles")->getData();
+      $docHumanInformationUploadedFiles = $form->get("docHumanInformationFiles")->getData();
+
+      $projectDir = $this->getParameter('brochures_directory');
+
+      $extInstitutionsAuthorizationFiles = $fileManager->uploadFiles($extInstitutionsAuthorizationUploadedFiles, $projectDir);
+      $docHumanInformationFiles = $fileManager->uploadFiles($docHumanInformationUploadedFiles, $projectDir);
+
+      $projectRequest->setExtInstitutionsAuthorizationFiles($extInstitutionsAuthorizationFiles);
+      $projectRequest->setDocHumanInformationFiles($docHumanInformationFiles);
+
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->persist($projectRequest);
+      $entityManager->flush();
+
+
+      $target = $form->get("form_target_input")->getData();
+
+      $route = $this->getTargetRoute($target);
+      $data = ['id' => $projectRequest->getId()];
+
+      return $this->redirectToRoute($route, $data);
     }
 
-    /**
-     * @Route("/new", name="project_request_new", methods={"GET","POST"})
-     */
-    public function new(Request $request, FileManager $fileManager): Response
-    {
-        $projectRequest = new ProjectRequest();
-        $form = $this->createForm(ProjectRequestType::class, $projectRequest);
-        $form->handleRequest($request);
+    return $this->render('project_request/new.html.twig', [
+                'project_request' => $projectRequest,
+                'form' => $form->createView()
+    ]);
+  }
 
-        if ($form->isSubmitted() && $form->isValid()) {
+  private function getTargetRoute($target) {
+    switch ($target) {
+      case "info":
+        $route = 'tab_general_info_request_edit';
+        break;
+      case "academic":
+        $route = 'tab_academic_request_info';
+        break;
+      case "ethic":
+        $route = 'tab_ethic_eval_request';
+        break;
 
-            $extInstitutionsAuthorizationUploadedFiles = $form->get("extInstitutionsAuthorizationFiles")->getData();
-            $docHumanInformationUploadedFiles = $form->get("docHumanInformationFiles")->getData();
+      default:
+        $route = 'tab_academic_request_info';
+        break;
+    }
+    return $route;
+  }
 
-            $projectDir = $this->getParameter('brochures_directory');
-            
-            $extInstitutionsAuthorizationFiles = $fileManager->uploadFiles($extInstitutionsAuthorizationUploadedFiles, $projectDir);
-            $docHumanInformationFiles = $fileManager->uploadFiles($docHumanInformationUploadedFiles, $projectDir);
+  /**
+   * @Route("/detalle/{id}", name="project_request_show", methods={"GET"})
+   */
+  public function show(ProjectRequest $projectRequest): Response {
+    return $this->render('project_request/show.html.twig', [
+                'project_request' => $projectRequest,
+    ]);
+  }
 
-            $projectRequest->setExtInstitutionsAuthorizationFiles($extInstitutionsAuthorizationFiles);
-            $projectRequest->setDocHumanInformationFiles($docHumanInformationFiles);
-            
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($projectRequest);
-            $entityManager->flush();
-            
-            $route = 'tab_academic_request_info';
-            $data = ['id' => $projectRequest->getId()];
-            
-            return $this->redirectToRoute($route, $data);
-        }
+  /**
+   * @Route("/{id}", name="project_request_edit", methods={"GET","POST"})
+   */
+  public function edit(Request $request, ProjectRequest $projectRequest, FileManager $fileManager): Response {
+    $form = $this->createForm(ProjectRequestType::class, $projectRequest);
+    $form->handleRequest($request);
 
-        return $this->render('project_request/new.html.twig', [
-            'project_request' => $projectRequest,
-            'form' => $form->createView()
-        ]);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $this->getDoctrine()->getManager()->flush();
+
+      $extInstitutionsAuthorizationUploadedFiles = $form->get("extInstitutionsAuthorizationFiles")->getData();
+      $docHumanInformationUploadedFiles = $form->get("docHumanInformationFiles")->getData();
+
+      $projectDir = $this->getParameter('brochures_directory');
+
+      $extInstitutionsAuthorizationFiles = $fileManager->uploadFiles($extInstitutionsAuthorizationUploadedFiles, $projectDir);
+      $docHumanInformationFiles = $fileManager->uploadFiles($docHumanInformationUploadedFiles, $projectDir);
+
+      $projectRequest->setExtInstitutionsAuthorizationFiles($extInstitutionsAuthorizationFiles);
+      $projectRequest->setDocHumanInformationFiles($docHumanInformationFiles);
+
+      $target = $form->get("form_target_input")->getData();
+
+      $route = $this->getTargetRoute($target);
+      $data = ['id' => $projectRequest->getId()];
+
+      return $this->redirectToRoute($route, $data);
     }
 
-    /**
-     * @Route("/detalle/{id}", name="project_request_show", methods={"GET"})
-     */
-    public function show(ProjectRequest $projectRequest): Response
-    {
-        return $this->render('project_request/show.html.twig', [
-            'project_request' => $projectRequest,
-        ]);
+    return $this->render('project_request/edit.html.twig', [
+                'project_request' => $projectRequest,
+                'form' => $form->createView(),
+    ]);
+  }
+
+  /**
+   * @Route("/{id}", name="project_request_delete", methods={"DELETE"})
+   */
+  public function delete(Request $request, ProjectRequest $projectRequest): Response {
+    if ($this->isCsrfTokenValid('delete' . $projectRequest->getId(), $request->request->get('_token'))) {
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->remove($projectRequest);
+      $entityManager->flush();
     }
 
-    /**
-     * @Route("/{id}", name="project_request_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, ProjectRequest $projectRequest, FileManager $fileManager): Response
-    {
-        $form = $this->createForm(ProjectRequestType::class, $projectRequest);
-        $form->handleRequest($request);
+    return $this->redirectToRoute('project_request_index');
+  }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-            
-            $extInstitutionsAuthorizationUploadedFiles = $form->get("extInstitutionsAuthorizationFiles")->getData();
-            $docHumanInformationUploadedFiles = $form->get("docHumanInformationFiles")->getData();
-
-            $projectDir = $this->getParameter('brochures_directory');
-            
-            $extInstitutionsAuthorizationFiles = $fileManager->uploadFiles($extInstitutionsAuthorizationUploadedFiles, $projectDir);
-            $docHumanInformationFiles = $fileManager->uploadFiles($docHumanInformationUploadedFiles, $projectDir);
-
-            $projectRequest->setExtInstitutionsAuthorizationFiles($extInstitutionsAuthorizationFiles);
-            $projectRequest->setDocHumanInformationFiles($docHumanInformationFiles);
-            
-            $route = 'tab_academic_request_info';
-            $data = ['id' => $projectRequest->getId()];
-            
-            return $this->redirectToRoute($route, $data);
-        }
-
-        return $this->render('project_request/edit.html.twig', [
-            'project_request' => $projectRequest,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="project_request_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, ProjectRequest $projectRequest): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$projectRequest->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($projectRequest);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('project_request_index');
-    }
 }
--- src/Form/AcademicRequestInfoType.php
+++ src/Form/AcademicRequestInfoType.php
@@ -8,57 +8,63 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use App\Entity\Criterion;
 use App\Repository\CriterionRepository;
 
 class AcademicRequestInfoType extends AbstractType {
 
-    public function buildForm(FormBuilderInterface $builder, array $options) {
-        $sippres = 'Vacio';
-        $br = '\n';
-        $builder
-                ->add('summaryObserv', TextareaType::class, [
-                    'attr' => ['class' => 'form-control', 'rows' => '4'],
-                    'help' => 'Máximo 1500 caracteres.<br>' . $sippres,
-                    'help_html' => true,
-                    'label_format' => 'Observaciones:',
-                ])
-                ->add('objetives', TextareaType::class, [
-                    'attr' => ['class' => 'form-control', 'rows' => '4'],
-                    'help' => 'Máximo 1500 caracteres.',
-                    'label' => 'Objetivos:',
-                ])
-                ->add('questions', TextareaType::class, [
-                    'attr' => ['class' => 'form-control', 'rows' => '4'],
-                    'help' => 'Máximo 1500 caracteres.',
-                    'label' => 'Preguntas:',
-                ])
-                ->add('hypothesis', TextareaType::class, [
-                    'attr' => ['class' => 'form-control', 'rows' => '4'],
-                    'help' => 'Máximo 1500 caracteres.',
-                    'label' => 'Hipotesis:',
-                ])
-                ->add('metodologyObserv', TextareaType::class, [
-                    'attr' => ['class' => 'form-control', 'rows' => '4'],
-                    'help' => 'Máximo 1500 caracteres.',
-                    'label' => 'Observaciones:',
-                ])
-                ->add('invType', EntityType::class, [
-                    'class' => Criterion::class,
-                    'multiple' => true,
-                    'expanded' => true,
-                    'query_builder' => function(CriterionRepository $repo) {
-                        return $repo->createPopulationQueryBuilder('investigationType');
-                    },
-                    'label' => 'Tipo de investigación:',
-                    'help' => '(Marque todas las opciones que sean necesarias)'
-        ]);
-    }
+  public function buildForm(FormBuilderInterface $builder, array $options) {
+    $sippres = 'Vacio';
+    $br = '\n';
+    $builder
+            ->add('form_target_input', HiddenType::class, [
+                'attr' => ['class' => 'form_target_input'],
+                'required' => false,
+                'mapped' => false
+            ])
+            ->add('summaryObserv', TextareaType::class, [
+                'attr' => ['class' => 'form-control', 'rows' => '4'],
+                'help' => 'Máximo 1500 caracteres.<br>' . $sippres,
+                'help_html' => true,
+                'label_format' => 'Observaciones:',
+            ])
+            ->add('objetives', TextareaType::class, [
+                'attr' => ['class' => 'form-control', 'rows' => '4'],
+                'help' => 'Máximo 1500 caracteres.',
+                'label' => 'Objetivos:',
+            ])
+            ->add('questions', TextareaType::class, [
+                'attr' => ['class' => 'form-control', 'rows' => '4'],
+                'help' => 'Máximo 1500 caracteres.',
+                'label' => 'Preguntas:',
+            ])
+            ->add('hypothesis', TextareaType::class, [
+                'attr' => ['class' => 'form-control', 'rows' => '4'],
+                'help' => 'Máximo 1500 caracteres.',
+                'label' => 'Hipotesis:',
+            ])
+            ->add('metodologyObserv', TextareaType::class, [
+                'attr' => ['class' => 'form-control', 'rows' => '4'],
+                'help' => 'Máximo 1500 caracteres.',
+                'label' => 'Observaciones:',
+            ])
+            ->add('invType', EntityType::class, [
+                'class' => Criterion::class,
+                'multiple' => true,
+                'expanded' => true,
+                'query_builder' => function(CriterionRepository $repo) {
+                  return $repo->createPopulationQueryBuilder('investigationType');
+                },
+                'label' => 'Tipo de investigación:',
+                'help' => '(Marque todas las opciones que sean necesarias)'
+    ]);
+  }
 
-    public function configureOptions(OptionsResolver $resolver) {
-        $resolver->setDefaults([
-            'data_class' => AcademicRequestInfo::class,
-        ]);
-    }
+  public function configureOptions(OptionsResolver $resolver) {
+    $resolver->setDefaults([
+        'data_class' => AcademicRequestInfo::class,
+    ]);
+  }
 
 }
