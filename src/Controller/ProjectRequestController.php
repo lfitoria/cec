5144a4f646e1ad5f9ffbb5c4e49871a2ed752dc1
lfitoria <lfitoria@eldomo.net>
2019-10-23 20:25:35 -0600

    pruebasServer-v5-cedulaestud-v2

--- src/Controller/ProjectRequestController.php
+++ src/Controller/ProjectRequestController.php
@@ -250,8 +250,9 @@ class ProjectRequestController extends AbstractController {
 
     $em = $this->getDoctrine()->getManager('oracle');
     $student = $externalDataManager->getStudentById($em, $studentId); //'B04278'
-
+    echo "<pre>"
     var_dump($student);
+    echo "</pre>"
     die();
 
     if ($student) {
