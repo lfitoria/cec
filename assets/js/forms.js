a98da5000fcef69958e2e6ddc32a592ee1ff67f6
lfitoria <luisjose@MacBook-Pro-de-luis.local>
2019-11-05 20:41:50 -0600

    test-unitId-v2

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -337,7 +337,7 @@ const $ = require('jquery');
 
               $("#project_request_title").val(response.projectData.dsc_proyecto);
               $("#project_request_projectUnit").val(response.projectData.dsc_unidad);
-              console.log(response.projectData.dsc_unidad);
+              console.log(response.projectData.id_unidad);
               $("#project_request_uacademica").val(response.projectData.id_unidad);
 
               $(".project_researchers_table").html("");
