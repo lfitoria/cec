a08dfd2efe3946ba115d9a13df407a0db26f1223
lfitoria <luisjose@MacBook-Pro-de-luis.local>
2019-11-05 23:57:36 -0600

    test-login-eval-v3

--- src/Services/User/UserManager.php
+++ src/Services/User/UserManager.php
@@ -126,19 +126,21 @@ class UserManager {
 
     // var_dump($this->user->getRole()->getId());
 
-    if($this->user->getRole()->getId() == 4 && $opt_eval_form == 1){
-      // $role = $this->em->getRepository(UsersRoles::class)->find(4);
-      // $this->user->setRole($role);
-      $objToken = new UsernamePasswordToken($this->user, null, 'main', $this->user->getRoles());
-    }else if($this->user->getRole()->getId() == 4 && $opt_eval_form == 0){
-      $role = $this->em->getRepository(UsersRoles::class)->find(3);
-      // $this->user->setRole($role);
-      $objToken = new UsernamePasswordToken($this->user, null, 'main', $role);
-    }else{
-      $objToken = new UsernamePasswordToken($this->user, null, 'main', $this->user->getRoles());
-    }
+    // if($this->user->getRole()->getId() == 4 && $opt_eval_form == 1){
+    //   // $role = $this->em->getRepository(UsersRoles::class)->find(4);
+    //   // $this->user->setRole($role);
+    //   $objToken = new UsernamePasswordToken($this->user, null, 'main', $this->user->getRoles());
+    // }else if($this->user->getRole()->getId() == 4 && $opt_eval_form == 0){
+    //   $role = $this->em->getRepository(UsersRoles::class)->find(3);
+    //   // $this->user->setRole($role);
+    //   $objToken = new UsernamePasswordToken($this->user, null, 'main', $role);
+    // }else{
+    //   $objToken = new UsernamePasswordToken($this->user, null, 'main', $this->user->getRoles());
+    // }
 
 
+    var_dump($this->user->getRoles());
+    die();
     // $objToken = new UsernamePasswordToken($this->user, null, 'main', $this->user->getRoles());
 
     // update user last login
