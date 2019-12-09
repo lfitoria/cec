6afbb358621a634237852b1a589c36328aa80506
lfitoria <lfitoria@eldomo.net>
2019-12-09 16:10:09 -0600

    api7

--- src/Controller/ProjectRequestController.php
+++ src/Controller/ProjectRequestController.php
@@ -629,7 +629,7 @@ class ProjectRequestController extends AbstractController {
   // $content = $response->getContent();
   //API Url
 // $url = 'https://sla_serviciosexternos.sdp.ucr.ac.cr/Ws_Certificaciones.svc/rest/Ws_Certificaciones';
- $url = 'https://pokeapi.co/api/v2/pokemon/ditto/';
+//  $url = 'https://pokeapi.co/api/v2/pokemon/ditto/';
 //Initiate cURL.
 $ch = curl_init($url);
  
@@ -641,19 +641,25 @@ $ch = curl_init($url);
 // );
  
 //Encode the array into JSON.
-$jsonDataEncoded = json_encode($jsonData);
+// $jsonDataEncoded = json_encode($jsonData);
  
 //Tell cURL that we want to send a POST request.
-curl_setopt($ch, CURLOPT_POST, 1);
+// curl_setopt($ch, CURLOPT_POST, 1);
  
 //Attach our encoded JSON string to the POST fields.
-curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+// curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
  
 //Set the content type to application/json
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+// curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
  
 //Execute the request
-$result = curl_exec($ch);
+// $result = curl_exec($ch);
+
+$base = 'https://pokeapi.co/api/v2/pokemon/';
+$id = 1;
+$data = file_get_contents($base.$id.'/');
+$result = json_decode($data);
+
   var_dump($result);
   echo "<hr>";
     echo "test";
