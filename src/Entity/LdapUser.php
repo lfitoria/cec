16935cf65116249d7e15ae6205416d93ace6f82b
lfitoria <lfitoria@eldomo.net>
2020-04-24 09:50:31 -0600

    testloginevaluador-v12

--- src/Entity/LdapUser.php
+++ src/Entity/LdapUser.php
@@ -115,12 +115,15 @@ class LdapUser implements UserInterface {
       $this->session = $_SESSION;
       var_dump($this->session);
       if(isset($this->session['isResearcher'])){
+        var_dump("entra");
+        var_dump($this->session);
         $role = new UsersRoles();
         $role->setId(3);
         $role->setDescription("ROLE_RESEARCHER");        
         return $role;
 
       }
+
       return $this->role;
     }
 
