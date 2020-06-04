3263ffe33edfc16cb14dc43ecc60b952766eca0c
lfitoria <lfitoria@eldomo.net>
2020-06-04 17:16:04 -0600

    pdfconfig-v3

--- src/Services/Utils/PdfManager.php
+++ src/Services/Utils/PdfManager.php
@@ -20,7 +20,7 @@ class PdfManager{
 
         return new PdfResponse(
             $this->pdf->getOutputFromHtml($html,array(
-                'size.pageSize' => 'Letter',
+                'pageSize' => 'Letter',
                 
                 )),
             $filename 
