f797c61b6ec42c2b59f3067d8c33f93e439e7bc9
lfitoria <lfitoria@eldomo.net>
2020-06-10 19:49:04 -0600

    php

--- src/Repository/AcademicRequestInfoRepository.php
+++ src/Repository/AcademicRequestInfoRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\AcademicRequestInfo;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method AcademicRequestInfo|null find($id, $lockMode = null, $lockVersion = null)
