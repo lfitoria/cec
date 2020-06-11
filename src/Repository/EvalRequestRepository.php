fb2e73fa21e99626f27424def25d6c27831095ee
lfitoria <lfitoria@eldomo.net>
2020-06-10 19:55:03 -0600

    php

--- src/Repository/EvalRequestRepository.php
+++ src/Repository/EvalRequestRepository.php
@@ -4,7 +4,8 @@ namespace App\Repository;
 
 use App\Entity\EvalRequest;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+// use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method EvalRequest|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +15,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class EvalRequestRepository extends ServiceEntityRepository {
 
-    public function __construct(RegistryInterface $registry) {
+    public function __construct(ManagerRegistry $registry) {
         parent::__construct($registry, EvalRequest::class);
     }
 
