ea47f9d840b1591b2e51094d12f59aa60d26f266
lfitoria <lfitoria@eldomo.net>
2020-06-12 11:13:45 -0600

    testpreval

--- src/Services/Utils/ExternalDataManager.php
+++ src/Services/Utils/ExternalDataManager.php
@@ -470,7 +470,7 @@ class ExternalDataManager {
       $results = $statement->fetchAll();
       
     } catch (\Exception $e) {
-      var_dump($e);
+      //var_dump($e);
       //  die();
       return null;
     }
